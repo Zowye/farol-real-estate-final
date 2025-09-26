94c42fe41b9e7c32a451e2f9c770fd75f2f64382
Zowye <caiozowye@gmail.com>
2025-09-26 14:23:12 -0300

    Changed nuxt configs

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,25 +1,17 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   srcDir: 'app',
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
   modules: ['vuetify-nuxt-module'],
 
-  // 🚀 Adiciona isso:
+  // ❌ remove isso (não precisa)
+  // nitro: { preset: 'node-server' },
+
   nitro: {
-    preset: 'node-server',
-  },
-
-  devServer: {
-    port: process.env.PORT ? Number(process.env.PORT) : 8080,
+    // Cloud Run injeta automaticamente process.env.PORT
+    // O Nitro padrão (node) já respeita isso
     host: '0.0.0.0',
-  },
-
-  vite: {
-    server: {
-      hmr: { protocol: 'ws', host: 'localhost', port: 24678 },
-      watch: { usePolling: true, interval: 300 }
-    }
+    port: process.env.PORT ? Number(process.env.PORT) : 8080,
   },
 
   runtimeConfig: {
